178995f0182e732719b8bd85ab8e06cd9c468499
stwilson123 <499502394@qq.com>
2018-09-05 16:20:58 +0800

    update .gitignore

---
+++ BlocksCore.Test/NetFramework/Collection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BlocksCore.Test.NetFramework
+{
+   
+    public class Collection
+    {
+        [Fact]
+        public void EnumableIsReadOnly()
+        {
+            IEnumerable<TestItem> listString = new List<TestItem>() { new TestItem() { number = 0}, new TestItem() { number = 1} };
+
+            //foreach (var item in listString)
+            //{
+            //    item = new TestItem() {  number = 3}; 
+            //}
+        }
+    }
+
+
+    class TestItem
+    {
+        public int number { get; set; }
+    }
+}
