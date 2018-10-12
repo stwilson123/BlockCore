520dcf74c770a271785d1c28939b601b18c203dd
stwilson123 <499502394@qq.com>
2018-10-12 10:11:58 -0400

    commit resource

---
+++ Blocks.Resources/ModuleDescition.cs
@@ -0,0 +1,10 @@
+using BlocksCore.Module.Abstractions.ModuleDescription;
+
+[assembly: Module(Name = "Resources",
+    Author = "BlocksAuthor",
+    Version = "1.0.0.0"
+)]
+
+[assembly: Feature(Id = "Resources"
+    
+)]
