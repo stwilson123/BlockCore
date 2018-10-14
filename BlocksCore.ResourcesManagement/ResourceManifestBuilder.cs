309b2324fe743f40338ad4b81e68fd8f2dd0be46
stwilson123 <499502394@qq.com>
2018-10-14 20:46:34 +0800

    bower resource update

---
+++ BlocksCore.ResourcesManagement/ResourceManifestBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlocksCore.ResourcesManagement
+{
+    public class ResourceManifestBuilder
+    {
+        public ResourceManifestBuilder()
+        {
+            ResourceManifests = new HashSet<IResourceManifest>();
+        }
+
+
+
+        internal HashSet<IResourceManifest> ResourceManifests { get; private set; }
+
+        public ResourceManifest Add()
+        {
+            var manifest = new ResourceManifest { Feature = Feature };
+            ResourceManifests.Add(manifest);
+            return manifest;
+        }
+
+        public void Add(IResourceManifest manifest)
+        {
+            ResourceManifests.Add(manifest);
+        }
+
+    }
+}
